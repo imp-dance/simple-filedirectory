29ea90a44617555b9fe41b9f71dfd12862fc3727
Haakon <haakon@underbakke.net>
2017-03-28 08:31:29 +0200

    External key&pass

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
+include('keypass.php');
 $msg = $_GET['n'];
 if ($msg == 1){
     $feed = "<span class='cmd-cont'>File successfully uploaded to <a target='_blank' class='filelink' href='http://cmd.underbakke.net/files/".$_GET['url']."'>".$_GET['url']."</a><br/>Type 'copy' to copy, or 'go' to redirect to file.";
 }
-if (@$_GET['k']=='a'){
+if (@$_GET[$kpk]==$kpp){
 ?><!doctype html>
 <html>
 <head>
